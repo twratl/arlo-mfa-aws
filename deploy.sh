53fd91b591c649dd592baaca2222dbe3c3b3be03
Thomas Rawley <thomas@rawley.net>
2020-06-22 12:49:00 -0400

    updated readme documentation and added region in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ sam build -t template.yaml
 
 sam deploy \
     --template-file ./.aws-sam/build/template.yaml \
-    --stack-name $1 \
+    --stack-name $2 \
     --capabilities CAPABILITY_IAM \
-    --parameter-overrides IncomingEmailAddress=$2 \
-    --s3-bucket $3
+    --parameter-overrides IncomingEmailAddress=$3 \
+    --s3-bucket $4 \\
+    --region $1
